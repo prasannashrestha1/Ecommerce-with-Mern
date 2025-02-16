8f849a1d274d6fff8b4ee8723c07befa02ce1581
prasannashrestha1 <97834918+prasannashrestha1@users.noreply.github.com>
2025-02-16 15:27:45 +0545

    managing layouts

--- client/src/components/Layout/UserLayout.jsx
+++ client/src/components/Layout/UserLayout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Header from "../Common/Header";
+import Footer from "../Common/Footer";
 
 const UserLayout = () => {
   return (
     <>
       {/* Header        */}
+      <Header />
 
       {/* Main Content        */}
       {/* Footer        */}
+      <Footer />
     </>
   );
 };
