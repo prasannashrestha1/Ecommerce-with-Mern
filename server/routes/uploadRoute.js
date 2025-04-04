4454f2fce0a318079fb3b1ce9c0db26017a6b766
prasannashrestha1 <97834918+prasannashrestha1@users.noreply.github.com>
2025-04-04 11:15:23 +0545

    settingup cloudinary

---
+++ server/routes/uploadRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import multer from "multer";
+import cloudinary from "cloudinary";
+import streamifier from "streamifier";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
